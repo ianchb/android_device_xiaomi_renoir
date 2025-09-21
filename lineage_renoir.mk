f55828fcb03e82495f8d3066ce6137035abf1213
ianchb <i@4t.pw>
2025-09-21 21:13:37 +0800

    renoir: Set DerpFest build type so that we don't need to export DERPFEST_BUILD_TYPE manually.
    Signed-off-by: ianchb <i@4t.pw>

--- lineage_renoir.mk
+++ lineage_renoir.mk
@@ -10,6 +10,8 @@ $(call inherit-product, device/xiaomi/renoir/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+DERPFEST_BUILD_TYPE := Community
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
